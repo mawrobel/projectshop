cb554203c2894f6848eedcba8627323d57214f72
Michał Wróbel <mik4llpl@gmail.com>
2020-08-02 22:10:34 +0200

    fix orders bug

--- order/views.py
+++ order/views.py
@@ -8,6 +8,7 @@ from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework import permissions
 
+
 @permission_classes((permissions.AllowAny,))
 def order_create(request):
     cart = Cart(request)
@@ -22,20 +23,7 @@ def order_create(request):
                                          quantity=item['quantity'])
             cart.clear()
             return render(request, 'orders/order/created.html', {'order': order})
-        else:
-            form = OrderForm()
-            context = {
-                'cart': cart,
-                'form': form,
-            }
-            return render(request, 'orders/order/create.html', context)
-
-
-@permission_classes((permissions.AllowAny,))
-class order_create_api(APIView):
-
-    def get(self, request):
-        cart = Cart(request)
+    else:
         form = OrderForm()
         context = {
             'cart': cart,
@@ -43,15 +31,4 @@ class order_create_api(APIView):
         }
         return render(request, 'orders/order/create.html', context)
 
-    def post(self, request):
-        cart = Cart(request)
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            order = form.save()
-            for item in cart:
-                OrderItem.objects.create(order=order,
-                                         product=item['product'],
-                                         price=item['price'],
-                                         quantity=item['quantity'])
-            cart.clear()
-            return render(request, 'orders/order/created.html', {'order': order})
+
